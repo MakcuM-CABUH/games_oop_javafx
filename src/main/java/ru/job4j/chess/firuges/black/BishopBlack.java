e60d025182e68f4ec6ea1c3bc948bdcb9b85f561
MakcuM-CABUH <job4j@mail.ru>
2025-11-19 17:21:07 +0500

    1

---
+++ src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,44 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell ps) {
+        position = ps;
+    }
+
+    @Override
+    public Cell position() {
+        return position;
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY());
+    }
+
+    @Override
+    public Cell[] way(Cell dest) {
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(String.format("Could not move by diagonal from %s to %s", position, dest));
+        }
+        int size = Math.abs(dest.getX() - position.getX());
+        Cell[] steps = new Cell[size];
+        int deltaX = (dest.getX() > position.getX()) ? 1 : -1;
+        int deltaY = (dest.getY() > position.getY()) ? 1 : -1;
+        for (int index = 0; index < size; index++) {
+            int newX = position.getX() + (index + 1) * deltaX;
+            int newY = position.getY() + (index + 1) * deltaY;
+            steps[index] = Cell.findBy(newX, newY);
+        }
+        return steps;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
---
+++ src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.KnightBlack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+
+        BishopBlack bishop = new BishopBlack(Cell.A1);
+        KnightBlack knight = new KnightBlack(Cell.C3);
+
+        logic.add(bishop);
+        logic.add(knight);
+
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.A1, Cell.E5);
+        });
+        assertThat(exception.getMessage()).isEqualTo("One of the cells Occupied!");
+    }
+
+    @Test
+    public void whenImpossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+
+        BishopBlack bishop = new BishopBlack(Cell.A1);
+        logic.add(bishop);
+
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.A1, Cell.E6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from A1 to E6");
+    }
+}
